7b3ff900233a4b793a6ba8217c50b37d474376cd
Gralux <gralux16@gmail.com>
2020-05-25 22:00:36 +0300

    AI

--- B20_Ex02/Player.cs
+++ B20_Ex02/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using System.Text;
 
 namespace B20_Ex02
@@ -7,8 +8,8 @@ namespace B20_Ex02
      class Player
      {
 
-          private string r_Name;
-          private ePlayerType r_ETypeOfPlayer;
+          private string m_Name;
+          private ePlayerType m_ETypeOfPlayer;
           private int m_Score; // NESHANE BASOF
           public enum ePlayerType
           {
@@ -18,8 +19,8 @@ namespace B20_Ex02
 
           public Player(string i_PlayerName, ePlayerType i_ETypeOfPlayer)
           {
-               r_Name = i_PlayerName;
-               r_ETypeOfPlayer = i_ETypeOfPlayer;
+               m_Name = i_PlayerName;
+               m_ETypeOfPlayer = i_ETypeOfPlayer;
                m_Score = 0;
 
           }
@@ -27,21 +28,67 @@ namespace B20_Ex02
           //Readonly optional
           public int Score
           {
-               get => m_Score;
-               set => m_Score = value;
-          }
+
+            get => m_Score;
+            set => m_Score = value;
+        }
           //Readonly optional
           public string Name
           {
-               get => r_Name;
-               set => r_Name = value;
-               
+              get => m_Name;
+              set => m_Name = value;
           }
 
           public ePlayerType enumPlayerType
           {
-               get => r_ETypeOfPlayer;
-               set => r_ETypeOfPlayer = value;
+              get => m_ETypeOfPlayer;
+              set => m_ETypeOfPlayer = value;
+          }
+
+          public Pair?[] ComputerMove(List <Cell> io_AvailableCards,Dictionary<int,List<Pair>> io_SeenCards)
+          {
+            Pair?[] resultMove = new Pair?[2];
+
+
+            foreach(List<Pair> item in io_SeenCards.Values)
+            {
+                    
+                if(item.Count == 2)
+                {
+                    resultMove[0] = item[0];
+                    resultMove[1] = item[1];
+                }
+
+            }
+
+            if(resultMove[0] == null)
+            {
+                Random randomObj = new Random();
+                int randomCard1 = randomObj.Next(io_AvailableCards.Count-1);
+                Cell chosenCard = io_AvailableCards[randomCard1];
+                resultMove[0] = chosenCard.Location;
+
+
+                if(io_SeenCards.TryGetValue(chosenCard.CellContent, out List<Pair> seenMatches))
+                {
+                    if(seenMatches.Count > 0 && !(chosenCard.Location.Equals(seenMatches[0])))
+                    {
+                        resultMove[1] = seenMatches[0];
+                    }
+                    else
+                    {
+                        int randomCard2 = randomObj.Next(io_AvailableCards.Count);
+                        while(randomCard2 == randomCard1)
+                        {
+                            randomCard2= randomObj.Next(io_AvailableCards.Count);
+                        }
+
+                        resultMove[1] = io_AvailableCards[randomCard2].Location;
+                    }
+                }
+            }
+
+            return resultMove;
           }
      }
 }
